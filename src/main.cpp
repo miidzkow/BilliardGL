c8de5a91c547ed34fa279021172ad272446e10eb
michal.idzkowski <michal.idzkowski@ulb.be>
2023-01-17 19:24:41 +0100

    Cleaning up the code

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include<glm/gtc/type_ptr.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 
 #include "camera.h"
@@ -15,19 +15,15 @@
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 
-// settings
-const unsigned int SCR_WIDTH = 1500;
-const unsigned int SCR_HEIGHT = 1500;
+// Display size
+const unsigned int SCR_WIDTH = 1000;
+const unsigned int SCR_HEIGHT = 1000;
 
-// camera
-Camera camera(glm::vec3(0.0f, 8.0f, 17.0f));
-float lastX = SCR_WIDTH / 2.0f;
-float lastY = SCR_HEIGHT / 2.0f;
-bool firstMouse = true;
+// Camera (with initial position)
+Camera camera(glm::vec3(0.0f, 10.0f, 20.0f));
 
 
 
@@ -38,7 +34,6 @@ glm::vec3 lightPos(0.0f, 15.0f, 0.0f);
 int main()
 {
     // glfw: initialize and configure
-    // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -46,7 +41,6 @@ int main()
 
 
     // glfw window creation
-    // --------------------
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "BillardGL", NULL, NULL);
     if (window == NULL)
     {
@@ -56,77 +50,64 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
-    // tell GLFW to capture our mouse
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // glad: load all OpenGL function pointers
-    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
-    stbi_set_flip_vertically_on_load(true);
 
     // configure global opengl state
-    // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
-    // build and compile shaders
-    // -------------------------
-    Shader ourShader("../models/room/roomShader.vs", "../models/room/roomShader.fs");
-
+    // Shaders (as for now, roomShader=tableShader but duplicated for future proofing)
+    Shader tableShader("../models/table/tableShader.vs", "../models/table/tableShader.fs");
     Shader roomShader("../models/room/roomShader.vs", "../models/room/roomShader.fs");
 
-    // load models
-    // -----------
-    Model ourModel("../models/table/pooltable.obj");
-
-    Model roomModel("../models/room/EmptyRoom(OBJ).obj");
+    // Models
+    Model tableModel("../models/table/pooltable.obj");
+    Model roomModel("../models/room/room.obj");
 
     const glm::vec3 light_pos = glm::vec3(0.0, 3.0, 0.0);
 
-    // render loop
-    // -----------
+    // Main render loop
     while (!glfwWindowShouldClose(window))
     {
 
         // input
-        // -----
         processInput(window);
 
         // render
-        // ------
         glClearColor(0.76f, 0.88f, 1.00f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // don't forget to enable shader before setting uniforms
-        ourShader.use();
+        tableShader.use();
         roomShader.use();
 
-
+        // Set everything for the table
         // light properties
         glm::vec3 lightColor(1.0f, 1.0f, 1.0f);
         glm::vec3 diffuseColor = lightColor   * glm::vec3(0.5f); // decrease the influence
         glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); // low influence
-        ourShader.setVector3f("light.ambient", ambientColor * 10.0f);
-        ourShader.setVector3f("light.diffuse", diffuseColor * 10.0f);
-        ourShader.setVector3f("light.specular", 1.0f, 1.0f, 1.0f);
+        tableShader.setVector3f("light.ambient", ambientColor * 7.0f); // multiplied to increase the intensity
+        tableShader.setVector3f("light.diffuse", diffuseColor * 7.0f);
+        tableShader.setVector3f("light.specular", 1.0f, 1.0f, 1.0f);
 
         // material properties
-        ourShader.setVector3f("material.ambient", 1.0f, 0.5f, 0.31f);
-        ourShader.setVector3f("material.diffuse", 1.0f, 0.5f, 0.31f);
-        ourShader.setVector3f("material.specular", 0.5f, 0.5f, 0.5f);
-        ourShader.setFloat("material.shininess", 32.0f);
+        tableShader.setVector3f("material.ambient", 1.0f, 0.5f, 0.31f);
+        tableShader.setVector3f("material.diffuse", 1.0f, 0.5f, 0.31f);
+        tableShader.setVector3f("material.specular", 0.5f, 0.5f, 0.5f);
+        tableShader.setFloat("material.shininess", 32.0f);
 
 
-        roomShader.setVector3f("light.ambient", ambientColor * 10.0f);
-        roomShader.setVector3f("light.diffuse", diffuseColor * 10.0f);
+        // Set everything for the room
+        // light properties
+        roomShader.setVector3f("light.ambient", ambientColor * 7.0f);
+        roomShader.setVector3f("light.diffuse", diffuseColor * 7.0f);
         roomShader.setVector3f("light.specular", 1.0f, 1.0f, 1.0f);
 
         // material properties
@@ -137,32 +118,33 @@ int main()
 
 
 
-        // view/projection transformations
+        // view & projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
-        ourShader.setMatrix4("projection", projection);
-        ourShader.setMatrix4("view", view);
+        tableShader.setMatrix4("projection", projection);
+        tableShader.setMatrix4("view", view);
 
         roomShader.setMatrix4("projection", projection);
         roomShader.setMatrix4("view", view);
 
 
-        ourShader.setVector3f("lightColor", 1.0f, 1.0f, 1.0f);
-        ourShader.setVector3f("light.position", lightPos);
-        ourShader.setVector3f("viewPos", camera.Position);
+        tableShader.setVector3f("lightColor", 1.0f, 1.0f, 1.0f);
+        tableShader.setVector3f("light.position", lightPos);
+        tableShader.setVector3f("viewPos", camera.Position);
 
         roomShader.setVector3f("lightColor", 1.0f, 1.0f, 1.0f);
         roomShader.setVector3f("light.position", lightPos);
         roomShader.setVector3f("viewPos", camera.Position);
 
 
-        // render the loaded model
+        // Render the pool table
         glm::mat4 pooltable = glm::mat4(1.0f);
         pooltable = glm::translate(pooltable, glm::vec3(0.0f, 0.0f, 0.0f)); // position in the scene
         pooltable = glm::scale(pooltable, glm::vec3(10.0f, 10.0f, 10.0f));     // scale
-        ourShader.setMatrix4("model", pooltable);
-        ourModel.Draw(ourShader);
+        tableShader.setMatrix4("model", pooltable);
+        tableModel.Draw(tableShader);
 
+        // Render the room
         glm::mat4 room = glm::mat4(1.0f);
         room = glm::translate(room, glm::vec3(0.0f, 0.0f, 0.0f)); // position in the scene
         room = glm::scale(room, glm::vec3(15.0f, 15.0f, 15.0f));     // scale
@@ -217,30 +199,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-// glfw: whenever the mouse moves, this callback is called
-void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
-{
-    float xpos = static_cast<float>(xposIn);
-    float ypos = static_cast<float>(yposIn);
-
-    if (firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-    lastX = xpos;
-    lastY = ypos;
-
-    camera.ProcessMouseMovement(xoffset, yoffset);
-}
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
-// ----------------------------------------------------------------------
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(static_cast<float>(yoffset));
